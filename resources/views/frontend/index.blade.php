feb42250ec871ce606448ce53b9d16da9edecbd2
vyz <1951120161@sv.ut.edu.vn>
2022-05-04 14:04:16 +0700

    add Meta Tags and Setup SEO Tags title, description, keyword

--- resources/views/frontend/index.blade.php
+++ resources/views/frontend/index.blade.php
@@ -1,5 +1,11 @@
 @extends('layouts.app')
 
+@section('title', "Web IT blogging Website")
+
+@section('meta_description', "Web IT blogging Website")
+
+@section('meta_keyword', "Web IT blogging Website")
+
 @section('content')
 <h4>Hello</h4>
 
